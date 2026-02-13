768048dd4de3a821e9c9d0f8954ae7df204f1728
roshanmahesh <roshanmahesh2006@gmail.com>
2026-02-13 00:12:43 -0500

    Implemented FileHandler (Part B) with tests and documentation    - Created FileHandler class for reading files from data folder    - Added 5 unit tests (all passing)    - Documented design in FileHandler.txt    -Fixed error with file sorting

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -9,6 +9,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class FileHandler {
     private static final String DATA_FOLDER = "data";
@@ -27,6 +28,7 @@ public class FileHandler {
                 }
             }
         }
+        Collections.sort(files);
         return files;
     }
 
