cc5aedbc3493f8c04019f83688187ab836448e18
David Wang <dbwang2005@gmail.com>
2026-02-13 14:47:52 -0500

    Final sanity checks by member c

--- src/test/java/ProgramControlTest.java
+++ src/test/java/ProgramControlTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.*;
 import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 class ProgramControlTest {
 
@@ -9,25 +8,25 @@ class ProgramControlTest {
     void getFileList() {
         String expectedList = "01  filea.txt\n02  fileb.txt\n03  filec.txt\n04  fileempty.txt\n";
         ProgramControl Handler = new ProgramControl();
-        assertEquals(expectedList, Handler.getFileList(), "List of files correct\n");
+        assertEquals(expectedList, Handler.getFileList(), "List of files incorrect.\n");
     }
 
     @Test
     void insertingBadFileNumber() throws IOException {
         ProgramControl Handler = new ProgramControl();
-        assertEquals("Please enter a number correlating to files.\n", Handler.getFileContents("00", null), "The file contents are correct!");
+        assertEquals("Please enter a number correlating to files.\n", Handler.getFileContents("00", null), "The file contents are incorrect.");
     }
 
     @Test
     void insertBadKey() throws IOException {
         ProgramControl Handler = new ProgramControl();
-        assertEquals("Invalid key file path.\n", Handler.getFileContents("01", "bad.txt"), "Proper exception was thrown");
+        assertEquals("Invalid key file path.\n", Handler.getFileContents("01", "bad.txt"), "Proper exception wasn't thrown");
     }
 
     @Test
     void emptyFileTest() throws IOException {
         ProgramControl Handler = new ProgramControl();
-        assertEquals("", Handler.getFileContents("04", null), "Empty file opens properly.");
+        assertEquals("", Handler.getFileContents("04", null), "Empty file doesn't open properly.");
     }
 
 }
